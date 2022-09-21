6cd070ba3d60fcee60010c14a6da1f3d098548a1
seemsartless <david.s.toronto@gmail.com>
2022-09-21 13:14:11 -0400

    Added some comments to libraries and videos

---
+++ midi_and_music/hello_musical_world.py
@@ -0,0 +1,11 @@
+#
+# hello_musical_world.py
+#
+# Explorations working with midi files
+#
+# https://www.youtube.com/watch?v=w732EXqmfZU
+#
+# https://pythonlang.dev/repo/rainbow-dreamer-musicpy/
+# https://pythonlang.dev/repo/mido-mido/
+#
+
